3191105a43014a246f579df76bc75ba707754548
TomRoberto <tom.roberto31@gmail.com>
2026-03-26 14:22:18 +0100

    Ajout d'un carousel

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ function App() {
       <Header />
       <main>
         <h2 style={{ backgroundColor: "gold" }}>Hello tout le monde</h2>
+        <section>
+          <h3>La section de Tom</h3>
+          <p>J'ai bien travaillé ajd</p>
+        </section>
       </main>
     </>
   );
