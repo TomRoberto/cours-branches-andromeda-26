38ad1802469d0efca656f7d5573bf0958a15f9db
TomRoberto <tom.roberto31@gmail.com>
2026-03-26 14:33:32 +0100

    changed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 const Header = () => {
-  return <header style={{ backgroundColor: "red" }}>Header</header>;
+  return (
+    <header style={{ backgroundColor: "red" }}>C'est mon composant</header>
+  );
 };
 
 export default Header;
